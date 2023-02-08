02eb1b3deb9965bbc398a1577f475218761773aa
William <webuntermench@protonmail.com>
2023-02-08 15:10:28 +0300

    add Profile page

--- src/features/users/routes/Profile/Profile.tsx
+++ src/features/users/routes/Profile/Profile.tsx
@@ -1,3 +1,45 @@
+import { useUser } from "@/lib/auth";
+
+import style from "./Profile.module.css";
+
+interface UserProperty {
+  label: string;
+  value: string;
+}
+
+const UserProperty = ({ label, value }: UserProperty) => {
+  return (
+    <div className={style["user__wrapper"]}>
+      <div className={style["user__elem"]}>{label}</div>
+      <div className={style["user__elem"]}>{value}</div>
+    </div>
+  );
+};
+
 export const Profile = () => {
-  return <h1>Profile</h1>;
+  const { data } = useUser();
+  return (
+    <section className={style["profile"]}>
+      <div className={style["profile__wrapper"]}>
+        <h1 className={style["profile__title"]}>Ваш профиль</h1>
+        <div className={style["profile__container"]}>
+          <div className={style["profile__head"]}>
+            <div className={style["head__left"]}>
+              <p className={style["profile__descr"]}>
+                Здесь вы можете редактировать свой профиль
+              </p>
+            </div>
+            <div></div>
+          </div>
+          <div className={style["profile__body"]}>
+            <UserProperty label={"Имя"} value={data!.firstName} />
+            <UserProperty label={"Фамилия"} value={data!.lastName} />
+            <UserProperty label={"Компания"} value={data!.organization} />
+            <UserProperty label={"Логин"} value={data!.login} />
+            <UserProperty label={"API-ключ"} value={data!.id} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
