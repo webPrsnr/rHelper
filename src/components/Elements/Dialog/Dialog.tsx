6d27d31fa0fb2623e7a4ccc578d9ceacfc6cedae
William <webuntermench@protonmail.com>
2023-02-17 13:03:04 +0300

    delete unused _Transition.Child_ props

--- src/components/Elements/Dialog/Dialog.tsx
+++ src/components/Elements/Dialog/Dialog.tsx
@@ -16,29 +16,13 @@ export const Dialog = ({ isOpen, onClose, children }: DialogProps) => {
     <>
       <Transition appear show={isOpen} as={Fragment}>
         <InterfaceDialog as="div" className={style["dialog"]} onClose={onClose}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
+          <Transition.Child as={Fragment}>
             <div className={style["dialog__bg"]} />
           </Transition.Child>
 
           <div className={style["dialog__overflow"]}>
             <div className={style["dialog__child"]}>
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
+              <Transition.Child as={Fragment}>
                 <InterfaceDialog.Panel className={style["dialog__panel"]}>
                   {children}
                 </InterfaceDialog.Panel>
