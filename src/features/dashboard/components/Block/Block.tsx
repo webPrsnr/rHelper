bfb4417ca39859b44de6512a26b39716dff70da2
William <webuntermench@protonmail.com>
2023-05-27 12:12:46 +0300

    decomposing Block component

--- src/features/dashboard/components/Block/Block.tsx
+++ src/features/dashboard/components/Block/Block.tsx
@@ -1,142 +1,86 @@
-import clsx from "clsx";
-import { useRef } from "react";
+import { memo, ReactNode, useRef } from "react";
 import { useDrag, useDrop } from "react-dnd";
-import type { Identifier } from "dnd-core";
-import { DragCard } from "../Card/Card";
-import { SingleCard, blockType, cardType } from "../../types";
+import { CardList, DragCard } from "../Card/Card";
+import { cardType } from "../../types";
 import { useCard, useMoveCard, useTransferBlock } from "../../api";
 import style from "./Block.module.css";
-import { BlockOptions } from "../BlockOptions/BlockOptions";
 import { Spinner } from "@/components/Elements";
+import { useBlockMutation } from "./useBlockMutation";
 
-interface BlockProps {
-  title: string;
-  children: (card: SingleCard, hIndex: string) => JSX.Element;
+const BlockWrapper = ({
+  children,
+  id,
+}: {
+  children: ReactNode;
   id: string;
-  userId: string;
-  hIndex: number;
-}
-
-interface DragBlock {
-  hIndex: number;
-  id: number;
-  type: string;
-}
-
-export const Block = ({ children, title, id, userId, hIndex }: BlockProps) => {
-  const cards = useCard(id);
+}) => {
   const cardMutation = useMoveCard();
-  const blockMutation = useTransferBlock();
-
-  const ref = useRef<HTMLDivElement>(null);
-
-  //TODO: Вынести в отдельный хук.
-  const [{ handlerId }, dropRef] = useDrop<
-    DragBlock,
-    void,
-    { handlerId: Identifier | null }
-  >(
-    {
-      accept: blockType.ELEMENT,
-      collect(monitor) {
-        return {
-          handlerId: monitor.getHandlerId(),
-        };
-      },
-      drop: (item) => {
-        const dragIndex = item.hIndex;
-        const dropIndex = hIndex;
-
-        if (dragIndex === dropIndex) {
-          return;
-        }
-        blockMutation.mutate({
-          currentBlockIndex: dragIndex,
-          nextBlockIndex: dropIndex,
-          token: userId,
-        });
-      },
-    },
-    [id]
-  );
-
-  const [{ isDragging }, dragRef] = useDrag(
-    {
-      type: blockType.ELEMENT,
-      item: () => {
-        return { id, hIndex };
-      },
-      collect: (monitor) => {
-        return {
-          isDragging: monitor.isDragging(),
-        };
-      },
-    },
-    [id]
-  );
-
-  //TODO: Продумать как прикрутить к Card
-  const [{ isHoveredCard, cardElem }, cardDrop] = useDrop<
+  const [, cardDrop] = useDrop<
     DragCard,
     void,
     { isHoveredCard: boolean; cardElem: DragCard }
-  >(
-    {
-      accept: cardType.ELEMENT,
-      drop: (item) => {
-        const dragIndex = item.blockId;
-        const dropIndex = id;
+  >({
+    accept: cardType.ELEMENT,
+    drop: (item) => {
+      const dragIndex = item.id;
+      const dropIndex = id;
 
-        if (dragIndex === dropIndex) {
-          return;
-        }
-        cardMutation.mutate({ blockId: id, cardId: item.id });
+      if (dragIndex === dropIndex) {
+        return;
+      }
+      cardMutation.mutate({ blockId: id, cardId: item.id });
+      console.log(item, dropIndex);
+    },
+  });
+  return <div ref={cardDrop}>{children}</div>;
+};
+
+interface BlockProps {
+  title: string;
+  id: string;
+  userId: string;
+  hIndex: number;
+  changeBlockPosition: (fItem: number, sItem: number) => void;
+}
+
+export const Block = memo(
+  ({ title, id, userId, hIndex, changeBlockPosition }: BlockProps) => {
+    console.log(title);
+    const blockRef = useRef<HTMLDivElement>(null);
+    const { drop, handlerId } = useBlockMutation({
+      blockRef: blockRef,
+      changeBlockPosition: changeBlockPosition,
+      hIndex: hIndex,
+      userId: userId,
+    });
+    const cards = useCard(id);
+
+    const [{ isDragging }, drag] = useDrag({
+      type: "block",
+      item: () => {
+        return { hIndex };
       },
       collect: (monitor) => ({
-        isHoveredCard: !!monitor.isOver(),
-        cardElem: monitor.getItem(),
+        isDragging: !!monitor.isDragging(),
       }),
-    },
-    [id]
-  );
+    });
 
-  const opacity = isDragging ? style["opacity"] : null;
-  const hover = isHoveredCard && cardElem.blockId !== id ? true : false;
-
-  dragRef(dropRef(ref));
-  return (
-    <div ref={cardDrop} style={{ position: "relative" }}>
-      <div
-        ref={ref}
-        className={clsx(style["block"], opacity)}
-        data-handler-id={handlerId}
-      >
-        {cards.status === "loading" ? (
-          <Spinner className={style["block__spinner"]} />
-        ) : (
-          <>
-            <div className={style["block__head"]}>
-              <h3>{title}</h3>
-              <BlockOptions id={id} />
-            </div>
-            {cards.data?.results.map((card) => children(card, card.h_index))}
-          </>
-        )}
-      </div>
-      {hover && (
+    drag(drop(blockRef));
+    return (
+      <BlockWrapper id={id}>
         <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            height: "100%",
-            width: "100%",
-            zIndex: 1,
-            opacity: 0.5,
-            backgroundColor: "yellow",
-          }}
-        ></div>
-      )}
-    </div>
-  );
-};
+          ref={blockRef}
+          className={style["block"]}
+          style={{ opacity: isDragging ? 0.5 : 1 }}
+          data-handler-id={handlerId}
+        >
+          {cards.status === "loading" ? (
+            <Spinner className={style["block__spinner"]} />
+          ) : (
+            <CardList id={id} title={title} />
+          )}
+        </div>
+      </BlockWrapper>
+    );
+  }
+);
