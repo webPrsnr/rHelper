e981235572ec152a383fcd192f7b644d42171a29
William <webuntermench@protonmail.com>
2023-05-27 12:10:37 +0300

    decomposing components

--- src/features/dashboard/routes/Panel/Panel.tsx
+++ src/features/dashboard/routes/Panel/Panel.tsx
@@ -1,61 +1,102 @@
 import { Spinner } from "@/components/Elements";
 import { useUser } from "@/lib/auth";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useBlock } from "../../api";
-import { Block, Card } from "../../components";
-import { SingleBlock, SingleCard } from "../../types";
+import { Block } from "../../components";
+import { SingleBlock } from "../../types";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import style from "./Panel.module.css";
 
-export const Panel = () => {
+const Board = () => {
   const { data: user } = useUser();
-  const userId = user?.id;
+  if (!user) {
+    return null;
+  }
 
-  const data = useBlock(userId!);
-
-  const renderCard = useCallback((card: SingleCard, hIndex: string) => {
-    return (
-      <Card
-        key={hIndex}
-        date={"14.05"}
-        grade={"Frontend"}
-        hIndex={hIndex}
-        id={card.resume_id}
-        name={card.resume_fields.name}
-        note={"Hello"}
-        salary={card.resume_fields.salary}
-        status={"pending"}
-        blockId={card.token_key}
-      />
-    );
-  }, []);
-
-  const renderBlock = useCallback(
-    (block: SingleBlock, index: number) => {
-      return (
-        <Block
-          key={block.column_id}
-          id={block.column_id}
-          userId={userId!}
-          hIndex={block.column_index}
-          title={block.column_title}
-        >
-          {renderCard}
-        </Block>
-      );
-    },
-    [userId]
-  );
+  const userId = user.id;
 
   return (
     <section className={style["panel"]}>
       <h2 className={style["panel__title"]}>Мониторинг</h2>
-      <div className={style["panel__wrapper"]}>
-        {data.status === "loading" ? (
-          <Spinner size="lg" className={style["panel__spinner"]} />
-        ) : (
-          data.data?.columns.map((block, index) => renderBlock(block, index))
-        )}
-      </div>
+      <Wrapper id={userId} />
     </section>
+  );
+};
+
+const Wrapper = ({ id }: { id: string }) => {
+  const data = useBlock(id);
+  return (
+    <div className={style["panel__wrapper"]}>
+      {data.status === "loading" ? (
+        <Spinner size="lg" className={style["panel__spinner"]} />
+      ) : !data.data ? null : (
+        <ColumnsWrapper columns={data.data.columns} id={id} />
+      )}
+    </div>
+  );
+};
+
+interface ColumnsWrapperProps {
+  columns: SingleBlock[];
+  id: string;
+}
+
+const ColumnsWrapper = ({ columns, id }: ColumnsWrapperProps) => {
+  const [controlledColumns, setColumns] = useState(columns);
+  const changeBlockPosition = useCallback(
+    (fItem: number, sItem: number) => {
+      //TODO
+      const objArray = [];
+      const indexArray = [];
+      for (let i = 0; controlledColumns.length >= i; i++) {
+        if (controlledColumns[i].column_index === fItem) {
+          objArray.push(controlledColumns[i]);
+          indexArray.push(i);
+          break;
+        }
+      }
+      for (let i = 0; controlledColumns.length >= i; i++) {
+        if (controlledColumns[i].column_index === sItem) {
+          objArray.push(controlledColumns[i]);
+          indexArray.push(i);
+          break;
+        }
+      }
+      const firstObj = objArray[0];
+      const secondObj = objArray[1];
+
+      const fIndex = indexArray[0];
+      const sIndex = indexArray[1];
+
+      const resArray = controlledColumns;
+      resArray[fIndex] = secondObj;
+      resArray[sIndex] = firstObj;
+      setColumns([...resArray]);
+    },
+    [id]
+  );
+  //TODO синхронизировать удаление блока react-query с controlledColumns;
+  return (
+    <>
+      {controlledColumns.map((block) => (
+        <Block
+          key={block.column_id}
+          id={block.column_id}
+          title={block.column_title}
+          userId={id}
+          hIndex={block.column_index}
+          changeBlockPosition={changeBlockPosition}
+        />
+      ))}
+    </>
+  );
+};
+
+export const Panel = () => {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Board />
+    </DndProvider>
   );
 };
